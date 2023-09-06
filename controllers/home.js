52725d79f87043ab25cf2392fc89e7f2672a622b
Christopher Munoz <christopherm0507@gmail.com>
2023-09-06 15:27:57 -0400

    UI updates and minor bug fixes

--- controllers/home.js
+++ controllers/home.js
@@ -449,13 +449,103 @@ module.exports = {
   
     res.render('haunted-mansion', { user: req.user, isDesktop, loggedIn, review: filteredReview, averageRating, disableReviewButton, comments: comments, filter: filter, pageName: pageName });
   },
-  
-  reviewSpaceMountain: (req, res) => {
+  getSevenDwarfsMineTrain: async (req, res) => {
     const loggedIn = req.isAuthenticated();
     const isDesktop = req.cookies.isDesktop === 'true'; // Access the cookie value
-    res.render('review-space-mountain', { isDesktop }, {
-      loggedIn: loggedIn,
+  // Initialize variables for filtering
+  let filter = req.query.filter || 'all'; // Default to 'all' filter
+  let filteredReview = [];
+  
+  if (filter === 'all') {
+    // Fetch all reviews without any specific filtering
+    filteredReview = await Review.find({ page: 'seven-dwarfs-mine-train' })
+      .populate('user')
+      .populate({
+        path: 'comments',
+        populate: { path: 'user' }
+      });
+  } else if (filter === 'your') {
+    // Fetch reviews submitted by the logged-in user
+    if (!req.user) {
+      // Redirect or handle as needed when user is not logged in
+      return res.redirect("back");
+    }
+    filteredReview = await Review.find({ user: req.user.id })
+      .populate('user')
+      .populate({
+        path: 'comments',
+        populate: { path: 'user' }
+      });
+  } else if (filter === 'highest-rated') {
+    // Fetch reviews sorted by highest rating
+    filteredReview = await Review.find({ page: 'seven-dwarfs-mine-train' })
+      .sort('-rating')
+      .populate('user')
+      .populate({
+        path: 'comments',
+        populate: { path: 'user' }
+      });
+  } else if (filter === 'most-critical') {
+  // Fetch reviews sorted by lowest rating
+  filteredReview = await Review.find({})
+    .sort('rating')
+    .populate('user')
+    .populate({
+      path: 'comments',
+      populate: { path: 'user' }
     });
+} else if (filter === 'most-recent') {
+  // Fetch reviews sorted by most recent
+  filteredReview = await Review.find({ page: 'seven-dwarfs-mine-train' })
+    .sort('-createdAt')
+    .populate('user')
+    .populate({
+      path: 'comments',
+      populate: { path: 'user' }
+    });
+}
+    const pageName = 'space-mountain'
+    const comments = await Comment.find({ page: 'seven-dwarfs-mine-train' })
+    .populate('user')
+    .populate({
+      path: 'comments', // Populate the 'comments' field for each review
+      populate: { path: 'user' } // Populate the 'user' field for comments
+    });
+    const review = await Review.find({ page: 'seven-dwarfs-mine-train' })
+      .populate('user')
+      .populate({
+        path: 'comments', // Populate the 'comments' field for each review
+        populate: { path: 'user' } // Populate the 'user' field for comments
+      });
+  
+    const totalReviews = review.length;
+    
+    // Calculate the total rating sum
+    const totalRatingSum = review.reduce((sum, review) => sum + review.rating, 0);
+    
+    // Calculate the average rating
+    const averageRating = totalRatingSum / totalReviews;
+  
+    // Initialize the disableReviewButton variable
+    let disableReviewButton = false;
+
+    let existingReview = null;
+    
+    if (req.user) {
+        existingReview = await Review.findOne({
+          page: 'seven-dwarfs-mine-train',
+          user: req.user.id,
+          reviewedBy: { $in: [req.user.id] },
+        });
+    }
+  
+    if (existingReview) {
+      // User has already submitted a review for this page
+      disableReviewButton = true;
+    }
+  
+    res.render('seven-dwarfs-mine-train', { user: req.user, isDesktop, loggedIn, review: filteredReview, averageRating, disableReviewButton, comments: comments, filter: filter, pageName: pageName });
+
   },
 };
 

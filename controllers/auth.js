9fdaebfd11f8a95c068ab4cb89b7ec4e47934ba9
Christopher Munoz <christopherm0507@gmail.com>
2023-09-23 18:53:44 -0400

    test

--- controllers/auth.js
+++ controllers/auth.js
@@ -87,14 +87,11 @@ exports.postSignup = (req, res, next) => {
   });
 
   // Append a unique identifier (timestamp) to the default profile picture URL
-  const defaultProfilePic = 'assets/image/Sunshine Attractions-logo/default-profile-pic.png';
-  const uniqueProfilePic = `${defaultProfilePic}?timestamp=${Date.now()}`;
-
   const user = new User({
     userName: req.body.userName,
     email: req.body.email,
     password: req.body.password,
-    profilePic: uniqueProfilePic
+    profilePic: null
   });
 
   User.findOne(
